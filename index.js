34dbc23518bf3abc7aae5631b777439b25942f23
lgmarchi <lucasgmarchi@gmail.com>
2024-05-20 13:08:01 -0300

    Adjust exports

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-import { wasm } from './Warp/warp/pkg/warp.js'
-import './Warp/extensions/warp-ipfs/pkg/warp_ipfs.js'
+import * as warp from './Warp/extensions/warp-ipfs/pkg/warp_ipfs.js';
+const init = warp.default
 
+export default init
+
+export * from './Warp/extensions/warp-ipfs/pkg/warp_ipfs.js';
